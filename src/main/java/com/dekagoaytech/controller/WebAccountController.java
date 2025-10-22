ff94faf6c4f80cf0be66a6c10ae8788978c64aaf
ily6ix <goitseonetrade@gmail.com>
2025-10-22 16:21:45 +0200

    Update Session management

--- src/main/java/com/dekagoaytech/controller/WebAccountController.java
+++ src/main/java/com/dekagoaytech/controller/WebAccountController.java
@@ -1,6 +1,7 @@
 package com.dekagoaytech.controller;
 
 import com.dekagoaytech.model.*;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -43,7 +44,9 @@ public class WebAccountController {
     }
 
     @GetMapping("/addAccount")
-    public String addAccount() {
+    public String addAccount(HttpSession session, Model model) {
+        model.addAttribute("name",session.getAttribute("name"));
+        model.addAttribute("id",session.getAttribute("idNumber"));
         return "addAccount"; // templates/dashboard.html
     }
 
@@ -60,8 +63,10 @@ public class WebAccountController {
                                 @RequestParam String city,
                                 @RequestParam String zip,
                                 @RequestParam long initialDeposit,
-                                Model model) {
-
+                                Model model,
+                                HttpSession session) {
+        session.setAttribute("name", firstName + " " + lastName);
+        session.setAttribute("idNumber", idNumber);
         // For now, just redirect to dashboard after submission
         return "redirect:/addAccount";
     }
